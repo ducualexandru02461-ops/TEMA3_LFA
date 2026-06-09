078c59dfecd5480c9c47d7dbc6f08e1c3a4f6e41
hammer_02461 <ducualexandru02461@gmail.com>
2026-06-09 15:21:50 +0300

    Update Cerinta1.py

--- Cerinta1.py
+++ Cerinta1.py
@@ -1,5 +1,6 @@
-import queue
 from collections import deque
+
+w = open("Cerinta1.out", "w")
 
 def citeste_pda ():
     pda = {}
@@ -25,6 +26,7 @@ def citeste_pda ():
         return stari, alfabet, nr_tranzitii, tranzitii, stare_initiala, simbol_initial_stiva, multime_stari_finale, mod_acceptare, cuvant_verificat
 
 print(citeste_pda())
+stari, alfabet, nr_tranzitii, tranzitii, stare_initiala, simbol_initial_stiva, multime_stari_finale, mod_acceptare, cuvant_verificat = citeste_pda()
 
 def simulator_pda(stare_initiala, simbol_initial_stiva, multime_stari_finale, cuvant_verificat, mod_acceptare, tranzitii, simbol_lambda = "lambda"):
     configuratie_initiala = (stare_initiala, 0, simbol_initial_stiva)
@@ -44,3 +46,40 @@ def simulator_pda(stare_initiala, simbol_initial_stiva, multime_stari_finale, cu
 
             if acceptat:
                 return True
+
+        if len(stiva_curenta) == 0: #daca nu am indeplinit conditiile de acceptare de mai sus, automatul s-a blocat
+            continue
+
+        varf_stiva = stiva_curenta[0]
+        mutari_posibile = []
+
+        if index_curent < len(cuvant_verificat):
+            litera_curenta = cuvant_verificat[index_curent]
+            cheie_normala = (stare_curenta, litera_curenta, varf_stiva)
+            mutari_posibile.append((cheie_normala, False))
+
+        cheie_lambda = (stare_curenta, simbol_lambda, varf_stiva)
+        mutari_posibile.append((cheie_lambda, True))
+
+        for cheie, este_lambda in mutari_posibile:
+            if cheie in tranzitii: #cautam o regula pentru aceasta configuratie
+                for stare_urmatoare, sir_pus_stiva in tranzitii[cheie]:
+                    if este_lambda:
+                        index_nou = index_curent #ramanem pe loc
+                    else:
+                        index_nou = index_curent + 1 #mergem mai departe
+                    if sir_pus_stiva == simbol_lambda:
+                        sir_de_adaugat = ""
+                    else:
+                        sir_de_adaugat = sir_pus_stiva
+                    stiva_noua = sir_de_adaugat + stiva_curenta[1:]
+                    configuratie_noua = (stare_urmatoare, index_nou, stiva_noua)
+                    if configuratie_noua not in vizitate:
+                        vizitate.add(configuratie_noua)
+                        coada.append(configuratie_noua)
+    return False
+
+if simulator_pda(stare_initiala, simbol_initial_stiva, multime_stari_finale, cuvant_verificat, mod_acceptare, tranzitii, simbol_lambda = "lambda"):
+    w.write("ACCEPTAT")
+else:
+    w.write("RESPINS")
